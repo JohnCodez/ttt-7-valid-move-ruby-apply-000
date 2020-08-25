6beca0801d5ba037736ee0583e1079492e0c72d4
John Exarhouleas <exarhouleasjohn@gmail.com>
2020-08-25 05:24:55 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,11 +11,5 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken? == true 
-    if board[index].between?(0, 8)
-      true
-    else false
-  end
-  else 
-    false
+  
   end
